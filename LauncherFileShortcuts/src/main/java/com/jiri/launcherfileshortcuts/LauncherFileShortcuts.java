55d62fb7e7c911568b944588881e777a9bb6ed4a
Jirigesi <jirigesi@gmail.com>
2019-12-11 10:57:47 -0800

    initial commit

---
+++ LauncherFileShortcuts/src/main/java/com/jiri/launcherfileshortcuts/LauncherFileShortcuts.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (©) 2019 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package com.jiri.launcherfileshortcuts;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.tjado.passwdsafe.FileListFragment;
+import net.tjado.passwdsafe.PasswdSafeApp;
+import net.tjado.passwdsafe.Preferences;
+import net.tjado.passwdsafe.StorageFileListFragment;
+import net.tjado.passwdsafe.lib.ApiCompat;
+import net.tjado.passwdsafe.lib.PasswdSafeUtil;
+
+public class LauncherFileShortcuts extends AppCompatActivity
+        implements FileListFragment.Listener,
+                   StorageFileListFragment.Listener
+{
+    public static final String EXTRA_IS_DEFAULT_FILE = "isDefFile";
+
+    private static final String TAG = "LauncherFileShortcuts";
+
+    private Boolean itsIsStorageFrag = null;
+    private boolean itsIsDefaultFile = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        PasswdSafeApp.setupDialogTheme(this);
+        super.onCreate(savedInstanceState);
+        setContentView(net.tjado.passwdsafe.R.layout.activity_launcher_file_shortcuts);
+
+        FragmentManager fragMgr = getSupportFragmentManager();
+        FragmentTransaction txn = fragMgr.beginTransaction();
+        //txn.replace(R.id.sync, new SyncProviderFragment());
+        txn.commit();
+
+        if (savedInstanceState == null) {
+            setFileChooseFrag();
+        }
+
+        Intent intent = getIntent();
+        if (!Intent.ACTION_CREATE_SHORTCUT.equals(intent.getAction())) {
+            finish();
+            return;
+        }
+
+        itsIsDefaultFile = intent.getBooleanExtra(EXTRA_IS_DEFAULT_FILE, false);
+        if (itsIsDefaultFile) {
+            setTitle(net.tjado.passwdsafe.R.string.default_file_to_open);
+        } else {
+            setTitle(net.tjado.passwdsafe.R.string.shortcut_file);
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setFileChooseFrag();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        FragmentManager mgr = getSupportFragmentManager();
+        Fragment frag = mgr.findFragmentById(net.tjado.passwdsafe.R.id.files);
+        boolean handled = (frag instanceof FileListFragment) &&
+                          frag.isVisible() &&
+                          ((FileListFragment) frag).doBackPressed();
+
+        if (!handled) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void openFile(Uri uri, String fileName)
+    {
+        if (itsIsDefaultFile || (uri != null)) {
+            Intent openIntent = null;
+            if (uri != null) {
+                openIntent = PasswdSafeUtil.createOpenIntent(uri, null);
+            }
+
+            Intent intent = new Intent();
+            intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, openIntent);
+            intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, fileName);
+            intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                            Intent.ShortcutIconResource.fromContext(
+                                    this, net.tjado.passwdsafe.R.mipmap.ic_launcher_passwdsafe));
+            setResult(Activity.RESULT_OK, intent);
+        }
+
+        finish();
+    }
+
+    @Override
+    public void createNewFile(Uri dirUri)
+    {
+    }
+
+    @Override
+    public boolean activityHasMenu()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean activityHasNoneItem()
+    {
+        return itsIsDefaultFile;
+    }
+
+    @Override
+    public boolean appHasFilePermission()
+    {
+        return false;
+    }
+
+    @Override
+    public void updateViewFiles()
+    {
+    }
+
+    /**
+     * Set the file chooser fragment
+     */
+    private void setFileChooseFrag()
+    {
+        SharedPreferences prefs = Preferences.getSharedPrefs(this);
+        boolean storageFrag =
+                ((ApiCompat.SDK_VERSION >= ApiCompat.SDK_KITKAT) &&
+                 !Preferences.getFileLegacyFileChooserPref(prefs));
+        if ((itsIsStorageFrag == null) || (itsIsStorageFrag != storageFrag)) {
+            PasswdSafeUtil.dbginfo(TAG, "setFileChooseFrag storage %b",
+                                   storageFrag);
+            Fragment frag;
+            if (storageFrag) {
+                frag = new StorageFileListFragment();
+            } else {
+                frag = new FileListFragment();
+            }
+            FragmentManager fragMgr = getSupportFragmentManager();
+            FragmentTransaction txn = fragMgr.beginTransaction();
+            txn.replace(net.tjado.passwdsafe.R.id.files, frag);
+            txn.commit();
+            itsIsStorageFrag = storageFrag;
+        }
+    }
+}
---
+++ authorizer/src/main/java/net/tjado/passwdsafe/otp/ScanActivity.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (©) 2019 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+
+package net.tjado.passwdsafe.otp;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import net.tjado.passwdsafe.R;
+import net.tjado.passwdsafe.otp.ScanFrameProcessor;
+import net.tjado.passwdsafe.otp.Token;
+
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.parameter.ScaleType;
+import io.fotoapparat.parameter.selector.FocusModeSelectors;
+import io.fotoapparat.view.CameraView;
+
+import static io.fotoapparat.parameter.selector.FocusModeSelectors.autoFocus;
+import static io.fotoapparat.parameter.selector.FocusModeSelectors.fixed;
+import static io.fotoapparat.parameter.selector.LensPositionSelectors.back;
+import static io.fotoapparat.parameter.selector.Selectors.firstAvailable;
+import static io.fotoapparat.parameter.selector.SizeSelectors.biggestSize;
+
+public class ScanActivity extends Activity
+{
+    private Fotoapparat fotoapparat;
+    private static ScanBroadcastReceiver receiver;
+
+    public class ScanBroadcastReceiver extends BroadcastReceiver
+    {
+        public static final String ACTION = "org.fedorahosted.freeotp.ACTION_CODE_SCANNED";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra("scanResult");
+            addTokenAndFinish(text);
+        }
+    }
+
+    public static boolean hasCamera(Context context) {
+        PackageManager pm = context.getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    private void addTokenAndFinish(String uri) {
+        Token token = null;
+        try {
+            token = new Token(uri);
+        } catch (Token.TokenUriInvalidException e) {
+            e.printStackTrace();
+        }
+
+        //do not receive any more broadcasts
+        this.unregisterReceiver(receiver);
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("uri", uri);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+/*
+        //check if token already exists
+        if (new TokenPersistence(ScanActivity.this).tokenExists(token)) {
+            finish();
+            return;
+        }
+
+        TokenPersistence.saveAsync(ScanActivity.this, token);
+        if (token == null || token.getImage() == null) {
+            finish();
+            return;
+        }
+
+        final ImageView image = (ImageView) findViewById(R.id.image);
+        Picasso.with(ScanActivity.this)
+                .load(token.getImage())
+                .placeholder(R.drawable.scan)
+                .into(image, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        findViewById(R.id.progress).setVisibility(View.INVISIBLE);
+                        image.setAlpha(0.9f);
+                        image.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                finish();
+                            }
+                        }, 2000);
+                    }
+
+                    @Override
+                    public void onError() {
+                        finish();
+                    }
+                });*/
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            this.unregisterReceiver(receiver);
+        }
+        catch (IllegalArgumentException e) {
+            // catch exception, when trying to unregister receiver again
+            // there seems to be no way to check, if receiver if registered
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        receiver = new ScanBroadcastReceiver();
+        this.registerReceiver(receiver, new IntentFilter(ScanBroadcastReceiver.ACTION));
+        setContentView(R.layout.activity_otp_scan);
+        CameraView cameraView = findViewById(R.id.camera_view);
+
+//        fotoapparat = Fotoapparat
+//                .with(this)
+//                .into(cameraView)
+//                .previewScaleType(ScaleType.CENTER_CROP)
+//                .photoSize(SizeSelectors.biggestSize())
+//                .lensPosition(LensPositionSelectors.back())
+//                .focusMode(Selectors.firstAvailable(
+//                        FocusModeSelectors.continuousFocus(),
+//                        autoFocus(),
+//                        fixed()
+//                ))
+//                .frameProcessor(new ScanFrameProcessor(this))
+//                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fotoapparat.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        fotoapparat.stop();
+    }
+}
